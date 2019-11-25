44e84959fc3ce17a200c54691eaa18d59b000cc7
Alexander Krimm <A.Krimm@gsi.de>
2019-11-25 15:42:44 +0100

    JMemoryBuddy function name change

--- chartfx-dataset/src/test/java/de/gsi/dataset/DataSetMemoryLeakTests.java
+++ chartfx-dataset/src/test/java/de/gsi/dataset/DataSetMemoryLeakTests.java
@@ -1,13 +1,12 @@
 package de.gsi.dataset;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.gsi.dataset.AxisDescription;
-import de.gsi.dataset.DataSet;
 import de.gsi.dataset.spi.DefaultAxisDescription;
 import de.gsi.dataset.spi.DefaultDataSet;
 import de.sandec.jmemorybuddy.JMemoryBuddy;
@@ -23,30 +22,24 @@ public class DataSetMemoryLeakTests {
     @Test
     public void LeakCheckerTest() {
         final Object referenced = new Object();
-        JMemoryBuddy.doMemTest(checker -> {
+        JMemoryBuddy.memoryTest(checker -> {
             Object notReferenced = new Object();
             checker.assertCollectable(notReferenced); // not referenced should be collectable
         });
 
-        boolean caughtException = false;
-        try {
-            JMemoryBuddy.doMemTest(checker -> {
+        AssertionError error = assertThrows(AssertionError.class, () -> {
+            JMemoryBuddy.memoryTest(checker -> {
                 checker.assertCollectable(referenced); // not collectable and should throw an exception
             });
-        } catch (Exception ex) {
-            caughtException = true;
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.atDebug().log("intercepted correct exception");
-            }
-        }
-        assertTrue(caughtException, "did not intercept correct exception");
+        });
+        assertTrue(error.getMessage().startsWith("Content of WeakReference was not collected"));
     }
 
     @Test
     public void MemoryLeakTest() {
         DataSet dataSet = new DefaultDataSet("TestAxisDescription");
 
-        JMemoryBuddy.doMemTest(checker -> {
+        JMemoryBuddy.memoryTest(checker -> {
             AxisDescription notReferenced = dataSet.getAxisDescription(0);
             dataSet.getAxisDescriptions().set(0, new DefaultAxisDescription(dataSet, "axis name", "axis unit"));
             checker.assertCollectable(notReferenced); // not referenced should be collectable
