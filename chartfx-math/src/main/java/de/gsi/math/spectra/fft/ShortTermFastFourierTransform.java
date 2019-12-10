cd4f2cfc8c5f5fd91abb186851f453e39f14b5d1
Alexander Krimm <A.Krimm@gsi.de>
2019-12-10 15:39:23 +0100

    Implement Complex SFFT

--- chartfx-math/src/main/java/de/gsi/math/spectra/fft/ShortTermFastFourierTransform.java
+++ chartfx-math/src/main/java/de/gsi/math/spectra/fft/ShortTermFastFourierTransform.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import de.gsi.dataset.DataSet;
 import de.gsi.dataset.DataSet3D;
 import de.gsi.dataset.spi.DoubleDataSet3D;
+import de.gsi.dataset.spi.MultiDimDoubleDataSet;
 import de.gsi.math.spectra.Apodization;
 import de.gsi.math.spectra.SpectrumTools;
 
@@ -18,6 +19,95 @@ import de.gsi.math.spectra.SpectrumTools;
  */
 public class ShortTermFastFourierTransform {
     private static final Logger LOGGER = LoggerFactory.getLogger(ShortTermFastFourierTransform.class);
+
+    public static DataSet3D getComplexSpectrogram(final MultiDimDoubleDataSet input, final int nQuantf, int nQuantt,
+            final Apodization apodization, final Padding padding) {
+        // validate input data
+        if (input.getDataCount(DIM_X) - nQuantf < nQuantt - 1) {
+            LOGGER.atWarn().addArgument(nQuantt).log("Not enough samples for requested time resolution: {}");
+        }
+
+        // set time axis
+        double[] timeAxis = new double[nQuantt];
+        int nData = (input.getDataCount(DIM_X) - nQuantf);
+        nData = nData < 1 ? 1 : nData;
+        for (int i = 0; i < nQuantt; i++) {
+            timeAxis[i] = input.get(DIM_X, Math.floorDiv(i * nData, nQuantt));
+        }
+
+        // set frequency axis
+        double fs = input.getDataCount(DIM_X) / (input.get(DIM_X, input.getDataCount(DIM_X) - 1) - input.get(DIM_X, 0));
+        double fStep = fs / nQuantf;
+        double[] frequencyAxis = new double[nQuantf];
+        for (int i = -nQuantf / 2; i < nQuantf / 2; i++) {
+            frequencyAxis[i + nQuantf / 2] = i * fStep;
+        }
+
+        // set amplitude data
+        double[][] amplitudeData = new double[nQuantf][nQuantt];
+        double amplitudeMin = Double.POSITIVE_INFINITY;
+        double amplitudeMax = Double.NEGATIVE_INFINITY;
+
+        // calculate scalogram
+        final DoubleFFT_1D fastFourierTrafo = new DoubleFFT_1D(nQuantf);
+        double[] raw = new double[2 * nQuantf];
+        double[] current = new double[nQuantf];
+        for (int i = 0; i < nQuantt; i++) {
+            for (int j = 0; j < nQuantf; j++) {
+                final int index = Math.floorDiv(i * nData, nQuantt) + j;
+                if (index >= input.getDataCount(DIM_X)) {
+                    switch (padding) {
+                    case ZERO:
+                        raw[2 * j] = 0;
+                        raw[2 * j + 1] = 0;
+                        break;
+                    case ZOH:
+                        raw[2 * j] = input.get(DIM_Y, nData - 1);
+                        raw[2 * j + 1] = input.get(DIM_Y + 1, nData - 1);
+                        break;
+                    case MIRROR:
+                        int mirroredIndex = nData - (nData - index) % nData;
+                        mirroredIndex = mirroredIndex < 0 ? -mirroredIndex : mirroredIndex;
+                        raw[2 * j] = input.get(DIM_Y, mirroredIndex);
+                        raw[2 * j + 1] = input.get(DIM_Y + 1, mirroredIndex);
+                        break;
+                    default:
+                    }
+                } else {
+                    raw[2 * j] = apodization.getIndex(j, nQuantf) * input.get(DIM_Y, index);
+                    raw[2 * j + 1] = apodization.getIndex(j, nQuantf) * input.get(DIM_Y + 1, index);
+                }
+            }
+            fastFourierTrafo.complexForward(raw);
+            current = SpectrumTools.computeMagnitudeSpectrum_dB(raw, true);
+            for (int j = 0; j < nQuantf; j++) {
+                amplitudeMin = Math.min(amplitudeMin, current[j]);
+                amplitudeMax = Math.max(amplitudeMax, current[j]);
+                if (j < nQuantf / 2) {
+                    amplitudeData[j + nQuantf / 2][i] = current[j];
+                } else {
+                    amplitudeData[j - nQuantf / 2][i] = current[j];
+                }
+            }
+        }
+
+        // initialize result dataset
+        DoubleDataSet3D result = new DoubleDataSet3D("SFFT(" + input.getName() + ")", timeAxis, frequencyAxis,
+                amplitudeData);
+        result.getMetaInfo().put("SFFT-nFFT", Integer.toString(nQuantf));
+        result.getMetaInfo().put("SFFT-nT", Integer.toString(nQuantt));
+
+        // Set Axis Labels and Units
+        final String timeUnit = input.getAxisDescription(DIM_X).getUnit();
+        result.getAxisDescription(DIM_X).set("Time", timeUnit, timeAxis[0], timeAxis[timeAxis.length - 1]);
+        final String freqUnit = timeUnit.equals("s") ? "Hz" : "1/" + timeUnit;
+        result.getAxisDescription(DIM_Y).set("Frequency", freqUnit, frequencyAxis[0],
+                frequencyAxis[frequencyAxis.length - 1]);
+        result.getAxisDescription(DIM_Z).set("Magnitude", "dB(" + input.getAxisDescription(DIM_Y).getUnit() + ")",
+                amplitudeMin, amplitudeMax);
+        LOGGER.atInfo().addArgument(result).log("result of complex sfft: {}");
+        return result;
+    }
 
     /**
      * Perform a Short term fourier transform
@@ -55,8 +145,10 @@ public class ShortTermFastFourierTransform {
 
         // set time axis
         double[] timeAxis = new double[nQuantt];
+        int nData = (input.getDataCount(DIM_X) - nQuantf);
+        nData = nData < 1 ? 1 : nData;
         for (int i = 0; i < nQuantt; i++) {
-            timeAxis[i] = input.get(DIM_X, Math.floorDiv(i * input.getDataCount(DIM_X), nQuantt));
+            timeAxis[i] = input.get(DIM_X, Math.floorDiv(i * nData, nQuantt));
         }
 
         // set frequency axis
@@ -68,35 +160,20 @@ public class ShortTermFastFourierTransform {
         }
 
         // set amplitude data
-        double[][] amplitudeData = new double[nQuantf/2][nQuantt];
+        double[][] amplitudeData = new double[nQuantf / 2][nQuantt];
         double amplitudeMin = Double.POSITIVE_INFINITY;
         double amplitudeMax = Double.NEGATIVE_INFINITY;
+
 
         // calculate scalogram
         final DoubleFFT_1D fastFourierTrafo = new DoubleFFT_1D(nQuantf);
         double[] raw = new double[nQuantf];
         double[] mean = new double[nQuantf / 2];
         double[] current = new double[nQuantf / 2];
-        final int nData = input.getDataCount(DIM_X);
         for (int i = 0; i < nQuantt; i++) {
             for (int j = 0; j < nQuantf; j++) {
-                final int index = Math.floorDiv(i * nData, nQuantt) + j - nQuantf / 2;
-                if (index < 0) {
-                    switch (padding) {
-                    case ZERO:
-                        raw[j] = 0;
-                        break;
-                    case ZOH:
-                        raw[j] = input.get(DIM_X, 0);
-                        break;
-                    case MIRROR:
-                        int mirroredIndex = -index % (2 * nData);
-                        mirroredIndex = mirroredIndex > nData ? 2 * nData - mirroredIndex : mirroredIndex;
-                        raw[j] = input.get(DIM_X, mirroredIndex);
-                        break;
-                    default:
-                    }
-                } else if (index >= nData) {
+                final int index = Math.floorDiv(i * nData, nQuantt) + j;
+                if (index >= input.getDataCount(DIM_X)) {
                     switch (padding) {
                     case ZERO:
                         raw[j] = 0;
@@ -107,6 +184,7 @@ public class ShortTermFastFourierTransform {
                     case MIRROR:
                         int mirroredIndex = nData - (nData - index) % nData;
                         mirroredIndex = mirroredIndex < 0 ? -mirroredIndex : mirroredIndex;
+                        raw[j] = input.get(DIM_Y, mirroredIndex);
                         break;
                     default:
                     }
@@ -135,9 +213,9 @@ public class ShortTermFastFourierTransform {
         final String freqUnit = timeUnit.equals("s") ? "Hz" : "1/" + timeUnit;
         result.getAxisDescription(DIM_Y).set("Frequency", freqUnit, frequencyAxis[0],
                 frequencyAxis[frequencyAxis.length - 1]);
-        result.getAxisDescription(DIM_Z).set("Magnitude", input.getAxisDescription(DIM_Y).getUnit(), amplitudeMin,
-                amplitudeMax);
-        LOGGER.atInfo().addArgument(result).log("result of sfft: {}");
+        result.getAxisDescription(DIM_Z).set("Magnitude", "dB(" + input.getAxisDescription(DIM_Y).getUnit() + ")",
+                amplitudeMin, amplitudeMax);
+        LOGGER.atInfo().addArgument(result).log("result of real sfft: {}");
         return result;
     }
 
